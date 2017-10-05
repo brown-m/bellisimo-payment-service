2272f162f116eb8fbd56dac11b0bf09229fab48a
LizBode <liz@meridianonline.co.za>
2017-10-05 11:17:47 +0200

    Add API for payment microservice

---
+++ src/main/java/com/payment/BellisimoPaymentServiceApplication.java
@@ -0,0 +1,12 @@
+package com.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BellisimoPaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BellisimoPaymentServiceApplication.class, args);
+	}
+}
