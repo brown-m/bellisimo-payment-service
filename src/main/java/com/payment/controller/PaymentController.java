dc9f9b752eaf5e32e000cb5cf96e4e66dbfd4337
LizBode <liz@meridianonline.co.za>
2017-10-17 13:11:02 +0200

    Add payment mocking service implementation

--- src/main/java/com/payment/controller/PaymentController.java
+++ src/main/java/com/payment/controller/PaymentController.java
@@ -1,5 +1,7 @@
 package com.payment.controller;
 
+import java.util.Random;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -16,10 +18,25 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class PaymentController {
 //	  @Autowired
 //	  private CartService cartService;
+		Random rand = new Random();
 		
 		@RequestMapping(value = "/", method = RequestMethod.GET)		
 		@ResponseBody
-		public Object index(){ 
-			return "Welcome to the payment service!";
+		public Object index(){
+			return processPayment();
+		}
+		
+		private boolean processPayment() {			
+//			int lucky = 0;
+//			for (int i = 0; i < 100; i++) {
+			if (rand.nextInt(100) < 95) {
+				System.out.println("Payment was processed successfully!");
+			    return true;
+			}
+			System.out.println("Payment failed!");
+			return false;
+//			}
+//			System.out.println(lucky);
+//			return false;
 		}
 }
